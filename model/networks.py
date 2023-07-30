426df2def003a9380c266cfa4767926d552eaf22
Melih <melihgorgulu98@gmail.com>
2023-07-30 17:11:59 +0000

    new architecture

--- model/networks.py
+++ model/networks.py
@@ -15,6 +15,13 @@ def get_input_shape():
     input_shape = (BATCH_SIZE, N_OF_CH, IMAGE_SIZE, IMAGE_SIZE)
     return input_shape
 
+def feature_l2_norm(f: torch.Tensor) -> torch.Tensor:
+    breakpoint()
+    epsilon = 1e-6
+    norm = (f.norm(dim=1, keepdim=True) + epsilon)
+    
+    return f / norm
+
 
 class BasicBackbone(nn.Module):
     """
@@ -69,15 +76,38 @@ class BasicBackbone(nn.Module):
         
 
 class FeatureMatcherHead(nn.Module):
-    def __init__(self):
+    def __init__(self, backbone_output_shape):
         super().__init__()
-        self.fcn1 = nn.Linear(32, 1)
+        _, _, h , w = backbone_output_shape
+        n_in_channel = h*w
+        self.conv1 = nn.Conv2d(in_channels=n_in_channel, out_channels=128, kernel_size=3, padding="same", stride=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding="same", stride=1)
+        self.fcn1 = nn.Linear(64*9*9, 512) # TODO: here is hardcoded, change it
+        self.fcn2 = nn.Linear(512, 1)
+        self.flatten = nn.Flatten()
+        
+        
+        self.batchnorm2d_1 = nn.BatchNorm2d(num_features=128)
+        self.batchnorm2d_2 = nn.BatchNorm2d(num_features=64)
+        self.maxpool2d = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         # self.fcn2 = nn.Linear(32, 1)
 
     def forward(self, x):
+        
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.maxpool2d(x)
+        x = self.batchnorm2d_1(x)
+        
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.maxpool2d(x)
+        x = self.batchnorm2d_2(x)
+        
+        x = self.flatten(x)
         x = self.fcn1(x)
-        #x = F.relu(x)
-        #x = self.fcn2(x)
+        x = F.relu(x)
+        x = self.fcn2(x)
         # We are using BCEWithLogitsLoss, so remove the last sigmoid layer
         # x = F.sigmoid(x)  # probability
         """This loss combines a `Sigmoid` layer and the `BCELoss` in one single class. 
@@ -89,44 +119,62 @@ class FeatureMatcherHead(nn.Module):
 
 
 class TransformPredictorHead(nn.Module):
-    def __init__(self):
+    def __init__(self, input_shape):
         super().__init__()
-        self.fcn = nn.Linear(32, 4)
+        _, ch, h, w = input_shape
+        self.fcn1 = nn.Linear(2*ch * h * w, 64)
+        self.fcn2 = nn.Linear(64, 32)
+        self.fcn3 = nn.Linear(32, 4)
         self.tanh = nn.Tanh()
 
     def forward(self, x):
-        x = self.fcn(x)  # 4 values
+        x = self.fcn1(x)
+        x = F.relu(x)
+        x = self.fcn2(x)
+        x = F.relu(x)
+        x = self.fcn3(x)  # Predict 4 values (cosx,sinx,tx,ty)
         x = self.tanh(x)
         return x
 
 
 class BasicSMNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, norm_feature: bool =True):
         super().__init__()
-        self.transform_head = TransformPredictorHead()
-        self.feature_matcher_head = FeatureMatcherHead()
+        self.norm_feature = norm_feature
         self.backbone = BasicBackbone()
         backbone_output_shape = self.backbone.get_output_shape()
-        _, ch, h, w = backbone_output_shape
+        self.transform_head = TransformPredictorHead(backbone_output_shape)
+        self.feature_matcher_head = FeatureMatcherHead(backbone_output_shape)
         self.flatten = nn.Flatten()
-        self.fcn1 = nn.Linear(2*ch * h * w, 64)
-        self.fcn2 = nn.Linear(64, 32)
-        self.dropout = nn.Dropout(p=0.5)
+
 
     def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
         # extract features
         f1 = self.backbone(x1)
         f2 = self.backbone(x2)
-
-        # Middle
+        # Transform prediction
         f = concat([f1, f2], dim=1)
         f_flattened = self.flatten(f)
-        x = self.fcn1(f_flattened)
-        x = F.relu(x)
-        x = self.fcn2(x)
-        x = F.relu(x)
-        # Feature matcher head
-        match_prob = self.feature_matcher_head(x)
         # Transform predictor head
-        transform_pred = self.transform_head(x)
+        transform_pred = self.transform_head(f_flattened)
+        # Feature matcher head
+        # TODO: Another way to calculate corr matrix, also try it out
+        # reshape feature maps for matrix multiplication
+        b,_,h1,w1 = f1.size()
+        b,_,h2,w2 = f2.size()
+        if self.norm_feature:
+            f1 = feature_l2_norm(f1)
+            f2 = feature_l2_norm(f2)
+        #f1 = f1.view(b,c,h1*w1).transpose(1,2) # size [b,c,h*w]
+        #f2 = f2.view(b,c,h2*w2) # size [b,c,h*w]
+        # perform matrix mult.
+        #correlation_map = torch.bmm(f1,f2)
+        # correlation_map = correlation_map.view(b,h1,w1,h2,w2).unsqueeze(1)
+        # With EINSUM
+        correlation_map = torch.einsum("bfhw,bfxy->bwhxy", f1, f2)
+        correlation_map = correlation_map.view(b, h1*w1, h2, w2)
+        if self.norm_feature:
+            correlation_map = feature_l2_norm(F.relu(correlation_map))
+        match_prob = self.feature_matcher_head(correlation_map)
+
         return match_prob, transform_pred
