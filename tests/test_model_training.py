c022f5a78e8762196f6a92eccd526d3959838e7a
Melih <melihgorgulu98@gmail.com>
2023-09-22 11:01:56 +0000

    random split function is added to data utils

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -16,7 +16,7 @@ from utils.config import get_train_config, get_data_config, get_stats_config
 import torch
 import torch.optim as optim
 
-from model.networks import BasicSMNetwork, SmNetwithResNetBackBone, SmNetCorrResNetBackBone
+from model.networks import BasicSMNetwork, SmNetwithResNetBackBone, SmNetCorrResNetBackBone, SmNetwithResNetBackBone_Small
 from training.losses import CombinedLoss
 from training.trainer import SMNetTrainer
 
@@ -27,64 +27,7 @@ from scripts.calculate_train_statistics import calculate_all_stats
 
 from torchvision.transforms import Compose
 
-import math
-from torch import default_generator, randperm
-from torch._utils import _accumulate
-from torch.utils.data.dataset import Subset
-
-import warnings
-
-def random_split(dataset, lengths,
-                 generator=default_generator):
-    r"""
-    Randomly split a dataset into non-overlapping new datasets of given lengths.
-
-    If a list of fractions that sum up to 1 is given,
-    the lengths will be computed automatically as
-    floor(frac * len(dataset)) for each fraction provided.
-
-    After computing the lengths, if there are any remainders, 1 count will be
-    distributed in round-robin fashion to the lengths
-    until there are no remainders left.
-
-    Optionally fix the generator for reproducible results, e.g.:
-
-    random_split(range(10), [3, 7], generator=torch.Generator().manual_seed(42))
-    random_split(range(30), [0.3, 0.3, 0.4], generator=torch.Generator().manual_seed(42))
-
-    Args:
-        dataset (Dataset): Dataset to be split
-        lengths (sequence): lengths or fractions of splits to be produced
-        generator (Generator): Generator used for the random permutation.
-    """
-    if math.isclose(sum(lengths), 1) and sum(lengths) <= 1:
-        subset_lengths: List[int] = []
-        for i, frac in enumerate(lengths):
-            if frac < 0 or frac > 1:
-                raise ValueError(f"Fraction at index {i} is not between 0 and 1")
-            n_items_in_split = int(
-                math.floor(len(dataset) * frac)  # type: ignore[arg-type]
-            )
-            subset_lengths.append(n_items_in_split)
-        remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
-        # add 1 to all the lengths in round-robin fashion until the remainder is 0
-        for i in range(remainder):
-            idx_to_add_at = i % len(subset_lengths)
-            subset_lengths[idx_to_add_at] += 1
-        lengths = subset_lengths
-        for i, length in enumerate(lengths):
-            if length == 0:
-                warnings.warn(f"Length of split at index {i} is 0. "
-                              f"This might result in an empty dataset.")
-
-    # Cannot verify that dataset is Sized
-    if sum(lengths) != len(dataset):    # type: ignore[arg-type]
-        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
-
-    indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[call-overload]
-    return [Subset(dataset, indices[offset - length : offset]) for offset, length in zip(_accumulate(lengths), lengths)]
-
-
+from data.data_utils import random_split
 
 def test_model_training(update_train_stats=False):
     
@@ -136,7 +79,7 @@ def test_model_training(update_train_stats=False):
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     # define the model
     # model = BasicSMNetwork()
-    model = SmNetwithResNetBackBone()
+    model = SmNetwithResNetBackBone_Small()
     # loss and optimizer
     criterion = CombinedLoss(transform_w=transform_loss_weight, match_w=match_loss_weight, 
                              translation_w=translation_loss_weight, rotation_w=rotation_loss_weight)
@@ -147,7 +90,7 @@ def test_model_training(update_train_stats=False):
     else:
         scheduler = None
     # define the trainer
-    experiment_name = "test_model_general"
+    experiment_name = "test_model_general_small_higher_lr_1e-3"
     logger_kwargs = {'update_step': 1, 'show': True}
     trainer = SMNetTrainer(model, criterion, optimizer, logger_kwargs=logger_kwargs,
                            device=device, train_stats_config=stats_config, experiment_name=experiment_name,
--- training/smnet_train.py
+++ training/smnet_train.py
@@ -1,5 +1,4 @@
 import os
-
 from data.data_model import ScanMatchingDataSet
 from data.transforms import Standardize, ResNet50_Transforms
 from data.data_model import DatasetFromSubset
@@ -7,9 +6,10 @@ from torch.utils.data import DataLoader
 from utils.config import get_train_config, get_data_config, get_stats_config
 import torch
 import torch.optim as optim
+from data.data_utils import random_split
 
 
-from model.networks import BasicSMNetwork, SmNetwithResNetBackBone
+from model.networks import BasicSMNetwork, SmNetwithResNetBackBone, SmNetwithResNetBackBone_Small
 from training.losses import CombinedLoss
 from training.trainer import SMNetTrainer
 
@@ -20,62 +20,6 @@ from scripts.calculate_train_statistics import calculate_all_stats
 
 from torchvision.transforms import Compose
 
-import math
-from torch import default_generator, randperm
-from torch._utils import _accumulate
-from torch.utils.data.dataset import Subset
-
-import warnings
-
-def random_split(dataset, lengths,
-                 generator=default_generator):
-    r"""
-    Randomly split a dataset into non-overlapping new datasets of given lengths.
-
-    If a list of fractions that sum up to 1 is given,
-    the lengths will be computed automatically as
-    floor(frac * len(dataset)) for each fraction provided.
-
-    After computing the lengths, if there are any remainders, 1 count will be
-    distributed in round-robin fashion to the lengths
-    until there are no remainders left.
-
-    Optionally fix the generator for reproducible results, e.g.:
-
-    random_split(range(10), [3, 7], generator=torch.Generator().manual_seed(42))
-    random_split(range(30), [0.3, 0.3, 0.4], generator=torch.Generator().manual_seed(42))
-
-    Args:
-        dataset (Dataset): Dataset to be split
-        lengths (sequence): lengths or fractions of splits to be produced
-        generator (Generator): Generator used for the random permutation.
-    """
-    if math.isclose(sum(lengths), 1) and sum(lengths) <= 1:
-        subset_lengths: List[int] = []
-        for i, frac in enumerate(lengths):
-            if frac < 0 or frac > 1:
-                raise ValueError(f"Fraction at index {i} is not between 0 and 1")
-            n_items_in_split = int(
-                math.floor(len(dataset) * frac)  # type: ignore[arg-type]
-            )
-            subset_lengths.append(n_items_in_split)
-        remainder = len(dataset) - sum(subset_lengths)  # type: ignore[arg-type]
-        # add 1 to all the lengths in round-robin fashion until the remainder is 0
-        for i in range(remainder):
-            idx_to_add_at = i % len(subset_lengths)
-            subset_lengths[idx_to_add_at] += 1
-        lengths = subset_lengths
-        for i, length in enumerate(lengths):
-            if length == 0:
-                warnings.warn(f"Length of split at index {i} is 0. "
-                              f"This might result in an empty dataset.")
-
-    # Cannot verify that dataset is Sized
-    if sum(lengths) != len(dataset):    # type: ignore[arg-type]
-        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
-
-    indices = randperm(sum(lengths), generator=generator).tolist()  # type: ignore[call-overload]
-    return [Subset(dataset, indices[offset - length : offset]) for offset, length in zip(_accumulate(lengths), lengths)]
 
 def train():
     train_config = get_train_config()
@@ -125,7 +69,7 @@ def train():
 
     # define the model
     # model = BasicSMNetwork()
-    model = SmNetwithResNetBackBone()
+    model = SmNetwithResNetBackBone_Small()
     # loss and optimizer
     criterion = CombinedLoss(transform_w=transform_loss_weight, match_w=match_loss_weight, 
                              translation_w=translation_loss_weight, rotation_w=rotation_loss_weight)
@@ -136,7 +80,7 @@ def train():
     else:
         scheduler = None
     # define the trainer
-    experiment_name = "20_09_2023_test_1"
+    experiment_name = "21_09_2023_test_1_small"
     logger_kwargs = {'update_step': 1, 'show': True}
     trainer = SMNetTrainer(model, criterion, optimizer, logger_kwargs=logger_kwargs,
                            device=device, train_stats_config=stats_config, experiment_name=experiment_name,
